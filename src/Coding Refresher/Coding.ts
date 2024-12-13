6028745c66209e36dd617981886643b8cba85cef
Gordon Deng <gordon.deng.1@us.af.mil>
2024-12-13 08:30:52 -0700

    added coding refresher section.

---
+++ src/Coding Refresher/Coding.ts
@@ -0,0 +1,14 @@
+//2) create a function that combines 2 words
+//4) create a class that has properties brand of type string and price of type number
+//6) add a honk method to the Car class that returns the string 'honk'.
+//8) add a title method to the Car class that takes a name argument and returns the string 'my name is <name> and my brand is <brand>'
+//10) Create an array called parking
+//12) Create a function called "totalPrice" that returns the total price of all cars.
+//14) Create a getData function that retrieves data from "https://jsonplaceholder.typicode.com/todos/1"
+// The returned data has the following object format
+// {
+//  "userId": 1,
+//  "id": 1,
+//  "title": "delectus aut autem",
+//  "completed": false
+// }
