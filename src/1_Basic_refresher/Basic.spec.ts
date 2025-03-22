1020d889362347f5a9331b28ef2e998ee67bd527
Gordon Deng <gordon.deng.1@us.af.mil>
2025-03-22 13:23:31 -0600

    update Basic instructions

--- src/1_Basic_refresher/Basic.spec.ts
+++ src/1_Basic_refresher/Basic.spec.ts
@@ -1,11 +1,12 @@
-//0) Review package.json basics. 
-//1) Write a test for a function that concatonates two strings.
-//3) Create a car class with properties brand of type string and price of type number
-//5) Add a method to the car class called honk that returns the string 'honk'
-//7) Add a method to the car class called title that takes a name argument and returns the string 'my name is <name> and my brand is <brand>'
-//9) Create an array called 'parking' that holds cars.
-//11) Create a function called "totalPrice" that returns the total price of all cars in the parking lot
-//13) Create a function called "getData" that calls an API with axios and returns a object with the key of 'data' and a value object with properties 'words' of type string and 'numbers' of type numbers
+//0) [ ] Review package.json basics. Understand what happens when 'npm run test' is executed. 
+//1) [ ] Write a test for a function that concatonates two strings.
+//3) [ ] Write a test to create an instance of a car class with properties brand of type string and price of type number
+//5) [ ] Write a test to see if there is a method to the car class called honk that returns the string 'honk'
+//7) [ ] Write a test to see if there is a method to the car class called title that takes a name argument and returns the string 'my name is <name> and my brand is <brand>'
+//9) [ ] Write a test to see if an array called 'parking' can holds instances of cars.
+//11) [ ] Write a test for a function called "totalPrice" that returns the total price of all cars in the parking lot given the array as an argument
+//13) [ ] Write a test for a function called "getData" that calls an API with axios and returns a object with the key of 'data' and a value object with properties 'words' of type string and 'numbers' of type numbers
+//13) Note: use this mock result structure {data: { words: "string", numbers: 5}} 
 //13a) verify if axios is installed via package.json file
 //13b) Install axios
 //13c) import axios, mock it, write the mock response, then write the test.
--- src/1_Basic_refresher/Basic.ts
+++ src/1_Basic_refresher/Basic.ts
@@ -1,10 +1,11 @@
-//2) create a function that concatenates/combines 2 words
-//4) create a class that has properties brand of type string and price of type number
-//6) add a honk method to the Car class that returns the string 'honk'.
-//8) add a title method to the Car class that takes a name argument and returns the string 'my name is <name> and my brand is <brand>'
-//10) Create an array called parking
-//12) Create a function called "totalPrice" that returns the total price of all cars.
-//14) Create a getData function that retrieves data from "https://jsonplaceholder.typicode.com/todos/1"
+//2) [ ] create a function that concatenates/combines 2 words
+//4) [ ] create a class called "Car" that has properties brand of type string and price of type number
+//6) [ ] add a honk method to the Car class that returns the string 'honk'.
+//8) [ ] add a title method to the Car class that takes a name argument and returns the string 'my name is <name> and my brand is <brand>'
+//10) [ ] Create an array called parking
+//12) [ ] Create a function called "totalPrice" that returns the total price of all cars.
+//14) [ ] Create a getData function that retrieves data from "https://jsonplaceholder.typicode.com/todos/1"
+//14 note:  axios returns data in { data: (....) } key.
 // The returned data has the following object format
 // {
 //  "userId": 1,
@@ -12,4 +13,4 @@
 //  "title": "delectus aut autem",
 //  "completed": false
 // }
-//14 note:  axios returns data in { data: } key.
+
